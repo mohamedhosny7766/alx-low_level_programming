355dd80c55e02c1c9145018862bb71e14c1a568b
root <mohamadhosny2542000@gimal.com>
2023-08-28 04:59:58 -0700

    a

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
+ * _memcpy - a function that copies memory area
+ * @dest: memory where is stored
+ * @src: memory where is copied
+ * *@n: number of bytes
+ *
+ * Return: copied memory with n byted changed
  */
-char *_strchr(char *s, char c)
-{
-	int i = 0;
 
-	for (; s[i] >= '\0'; i++)
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		dest[r] = src[r];
+		n--;
 	}
-	return (0);
+	return (dest);
 }
